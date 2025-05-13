81e9ad9f895a496d8e5d587dc8628980dca5e66d
kou <kou.matsumoto.jp@gmail.com>
2025-05-13 23:06:16 +0900

    refactor: rename ClogFormat folder to Domain
    - Renamed src/ClogFormat to src/Domain
    - Updated all import paths referencing ClogFormatService
    - Verified and fixed related test and implementation files
    - Ran code formatting and CI to ensure consistency and correctness

--- src/HomePanel/HomePanelService.ts
+++ src/HomePanel/HomePanelService.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as vscode from "vscode";
-import { ClogFormatService } from "../ClogFormat/ClogFormatService";
+import { ClogFormatService } from "../Domain/ClogFormatService";
 
 export class HomePanelService {
   /**
